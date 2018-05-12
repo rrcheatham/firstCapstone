ca0959c58fff89cf8ac8ce410166cdd9579d1990
rrcheatham <rrcheatham@gmail.com>
2018-05-12 06:54:36 -0400

    final commit before submission

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const REC_URL = "https://api.themoviedb.org/3/movie/";
 const INFO_URL = "http://www.omdbapi.com";
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
+//get film ID from the movieDB database to confirm intial search
 function getFilmID(title, callback) {
   const settings = {
     url: ID_URL,
@@ -18,6 +19,7 @@ function getFilmID(title, callback) {
   $.ajax(settings);
 }
 
+// rendering for each result of ID search with title and release year
 function renderIdResults(result) {
   var releaseYear = result.release_date.slice(0, 4);
   return `
@@ -28,12 +30,21 @@ function renderIdResults(result) {
     `;
 }
 
+//render error message of return of no results
 function renderErrMsg() {
   return `
     <p>Sorry we could not find that title, check your spelling or try a different film.</p>
   `;
 }
 
+function renderRecErr() {
+  return `
+    <p>Sorry there are no recommendations for this film, please try another title.</p>
+  `;
+}
+
+//if movieDB returns results pass them to the render function to create array and insert in DOM
+//if movieDB returns 0 results render error message and insert in DOM
 function displayIDSearch(data) {
   if (data.total_results !== 0) {
     const results = data.results.map((item, index) => renderIdResults(item));
@@ -44,6 +55,15 @@ function displayIDSearch(data) {
   }
 }
 
+//clear search field on click
+function clearSearchField() {
+  $('.js-query').on('click focus', event => {
+      $('.js-query').val('');
+    });
+}
+
+//intial submit event listener that calls movieDB API and passes it the title entered
+//scrolls down to results on submit
 function watchForSubmit() {
   $('.js-title-search').submit(event => {
       event.preventDefault();
@@ -51,9 +71,13 @@ function watchForSubmit() {
       const query = queryTarget.val();
       queryTarget.val('');
       getFilmID(query, displayIDSearch);
+      $('html, body').animate({
+        scrollTop: $('.js-resultsBox').offset().top,
+      }, 1000);
     });
 }
 
+//call to movieDB for list of recommendations based on filmID
 function callRecommendationAPI(filmID, callback) {
   const settings = {
     url: `https://api.themoviedb.org/3/movie/${filmID}/recommendations`,
@@ -67,6 +91,7 @@ function callRecommendationAPI(filmID, callback) {
   $.ajax(settings);
 }
 
+//render recommendation results
 function renderRecResults(result) {
   var releaseYear = result.release_date.slice(0, 4);
   return `
@@ -77,12 +102,19 @@ function renderRecResults(result) {
   `;
 }
 
+//create of array of data returned by API call and passed to render function, insert in DOM
 function displayRecommendations(data) {
-  const results = data.results.map((item, index) => renderRecResults(item));
-  $('.js-resultsBox').html(results);
-  watchForResultClick();
+  console.log(data.results.length);
+  if (data.results.length !== 0) {
+    const results = data.results.map((item, index) => renderRecResults(item));
+    $('.js-resultsBox').html(results);
+    watchForResultClick();
+  } else {
+    $('.js-resultsBox').html(renderRecErr);
+  }
 }
 
+//event listener for click on get recommendations button for each title
 function watchForTitleClick() {
   $('button.js-id-button').click(event => {
       event.preventDefault();
@@ -91,6 +123,7 @@ function watchForTitleClick() {
     });
 }
 
+//call to open movie DB for info on recommended film
 function callFilmInfoAPI(title, callback) {
   const settings = {
     url: INFO_URL,
@@ -107,6 +140,7 @@ function callFilmInfoAPI(title, callback) {
   $.ajax(settings);
 }
 
+//call to YouTube api for Movieclips results of selected recommendation
 function callClipsAPI(title, callback) {
   const settings = {
     url: YOUTUBE_SEARCH_URL,
@@ -122,15 +156,18 @@ function callClipsAPI(title, callback) {
   $.ajax(settings);
 }
 
+//render for each film result
 function renderFilmClips(result) {
   return `
     <div class="film-clips">
       <a target="_blank" href='http://www.youtube.com/watch?v=${result.id.videoId}'><img src='${result.snippet.thumbnails.default.url}' alt="clip thumbnail"></a>
       <h5>${result.snippet.title}</h5>
     </div>
-  `
+  `;
 }
 
+//render and display film info, no array of results since searching by ID
+//added customized links for stream services search based on title
 function displayFilmInfo(data) {
   const results = `
     <button type="button" class="js-return">Return To Results</button>
@@ -166,12 +203,15 @@ function displayFilmInfo(data) {
   $('.js-mainBox').addClass('hidden');
 }
 
+//pass youTube data to render function and insert array into DOM
 function displayFilmClips(data) {
   const results = data.items.map((item, index) => renderFilmClips(item));
+  console.log(results);
   $('.js-film-clips').append(results);
   returnToResults();
 }
 
+//event listener for Get Info button click, calls info and youTube APIs
 function watchForResultClick() {
   $('.js-rec-button').click(event => {
     event.preventDefault();
@@ -181,19 +221,26 @@ function watchForResultClick() {
   });
 }
 
+//event listener on return to results button
 function returnToResults() {
   $('.js-return').click(event => {
     event.preventDefault();
     $('.js-resultsBox').removeClass('hidden');
     $('.js-mainBox').removeClass('hidden');
     $('.js-infoBox').html('');
+    $('html, body').animate({
+      scrollTop: $('.js-resultsBox').offset().top,
+    }, 1000);
   });
 }
 
+//intial listeners for title submit
 function runListeners() {
+  clearSearchField();
   watchForSubmit();
-  watchForTitleClick();
-  returnToResults();
+
+  //  watchForTitleClick();
+  //  returnToResults();
 }
 
 $(runListeners);
